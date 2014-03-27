70f2290615069467a439b0ffe005dbddc0ab5f4d
Matias Hernandez <matias.hernandez@a-dedo.cl>
2014-03-27 19:42:40 -0300

    updated web

--- dist/web.js
+++ dist/web.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express();
 
 
-app.use(gzippo.staticGzip("" + __dirname));
+// app.use(gzippo.staticGzip("" + __dirname));
+app.use(express.static(__dirname));
 var port = process.env.PORT || 3000;
 app.listen(port);
 console.log("Listening in " + port);
