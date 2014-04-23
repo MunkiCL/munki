c23710defb2d9ee58c964fc5b235238c0e09c2e5
Matias Hernandez Arellano <matiasfh@gmail.com>
2014-04-23 20:25:52 -0300

    first munki gh-pages commit

---
+++ web.js
@@ -0,0 +1,9 @@
+var gzippo = require('gzippo');
+var express = require('express');
+var app = express();
+
+
+app.use(gzippo.staticGzip(__dirname));
+var port = process.env.PORT || 3000;
+app.listen(port);
+console.log("Listening in " + port);
