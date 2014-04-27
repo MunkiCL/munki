3caedc6e5827f33454fa88b8f3cbec3612e72e0c
Matias Hernandez Arellano <matiasfh@gmail.com>
2014-04-27 16:35:59 -0400

    reinit

--- dist/web.js
+++
@@ -1,9 +0,0 @@
-var gzippo = require('gzippo');
-var express = require('express');
-var app = express();
-
-
-app.use(gzippo.staticGzip(__dirname));
-var port = process.env.PORT || 3000;
-app.listen(port);
-console.log("Listening in " + port);
