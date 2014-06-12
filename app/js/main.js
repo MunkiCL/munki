28b0b8d4d18a7be62fd4618b8ac435b95f8441a4
Matías Hernández Arellano <matiasfh@gmail.com>
2014-06-12 17:27:45 -0400

    New slide for proyectos section

--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,43 @@
-$(window).scroll(function(){
-	if($(this).scrollTop() > 10){
-		$('.welcome').addClass('blur');
-	}else{
-		$('.welcome').removeClass('blur');
-	}
-    if($(this).scrollTop() > 625){
-        $('header').slideDown();
+$(document).ready(function(){
+	$(window).scroll(function(){
+		if($(this).scrollTop() > 10){
+			$('.welcome').addClass('blur');
+		}else{
+			$('.welcome').removeClass('blur');
+		}
+	    if($(this).scrollTop() > 625){
+	        $('header').slideDown();
 
-    }else{
-        $('header').slideUp();
+	    }else{
+	        $('header').slideUp();
 
-    }
+	    }
+	});
+
+	
+
+
+
+	var slides = $('.slide');
+
+	var glide = $('.slider').glide({
+		arrowRightText: '',
+		arrowLeftText: '',
+		autoplay: false,
+		circular: false,
+		navigation:false
+		// beforeTransition: function() {
+		// 	$(slides).eq(-this.currentSlide).removeClass('fadeInUpBig').addClass('fadeOutDownBig');
+
+		// },
+		// afterTransition: function() {
+		// 	$(slides).eq(-this.currentSlide).removeClass('fadeOutDownBig').addClass('fadeInUpBig');
+		// }
+	});
+	
 });
+
+
 
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
